cb2c4d7b1425a346f563518070cbb4b828f0fb9c
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-07 22:08:08 -0600

    feat(api): Add article model
    Slugify the title

---
+++ models/Article.ts
@@ -0,0 +1,80 @@
+import slugify from '@sindresorhus/slugify';
+import mongoose, { Document, Schema } from 'mongoose';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
+import { User } from './User';
+
+export interface Article extends Document {
+  title: string;
+  slug: string;
+  subtitle: string;
+  draft: boolean;
+  body: string;
+  tags: string[];
+  author: User;
+}
+
+const ArticleSchema = new Schema<Article>(
+  {
+    title: {
+      type: Schema.Types.String,
+      required: true,
+      index: true,
+    },
+    slug: {
+      type: Schema.Types.String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    subtitle: {
+      type: Schema.Types.String,
+      required: true,
+      trim: true,
+    },
+    draft: {
+      type: Schema.Types.Boolean,
+      default: true,
+    },
+    body: {
+      type: Schema.Types.String,
+      required: true,
+      trim: true,
+    },
+    tags: [
+      {
+        type: Schema.Types.String,
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+ArticleSchema.plugin(mongooseUniqueValidator, { message: 'already exists' });
+ArticleSchema.pre<Article>('validate', function(next) {
+  this.slug =
+    slugify(this.title) +
+    '-' +
+    ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
+
+  next();
+});
+ArticleSchema.methods.toJSON = function(this: Article) {
+  const article = this.toObject();
+
+  delete article.__v;
+  delete article._id;
+  article.author = this.author.toJSON();
+
+  return article;
+};
+
+if (process.env.NODE_ENV !== 'production' && 'Article' in mongoose.models) {
+  delete mongoose.models.Article;
+}
+
+export const Article = mongoose.model<Article>('Article', ArticleSchema);
