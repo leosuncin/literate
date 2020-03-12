7f7e9d27cac838c5be5319eec8e36ef67a8cd0b6
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-11 21:34:02 -0600

    feat(api): Edit a comment by id

--- pages/api/article/[slug]/comment/[id].ts
+++ pages/api/article/[slug]/comment/[id].ts
@@ -1,6 +1,12 @@
-import { NOT_FOUND } from 'http-status-codes';
-import { connectDB, validateMethod } from 'middlewares';
+import { FORBIDDEN, NOT_FOUND } from 'http-status-codes';
+import {
+  connectDB,
+  validateBody,
+  validateMethod,
+  withAuthentication,
+} from 'middlewares';
 import { Comment } from 'models';
+import { CommentSchema } from 'schemas';
 import { NextHttpHandler } from 'types';
 
 const showCommentHandler: NextHttpHandler = async (req, res) => {
@@ -14,5 +20,36 @@ const showCommentHandler: NextHttpHandler = async (req, res) => {
 
   return res.json(comment.toJSON());
 };
+const editCommentHandler: NextHttpHandler = async (req, res) => {
+  const comment = await Comment.findById(req.query.id).populate('author');
 
-export default validateMethod('GET', connectDB(showCommentHandler));
+  if (!comment)
+    return res.status(NOT_FOUND).json({
+      statusCode: NOT_FOUND,
+      message: `Not found any comment with id: ${req.query.id}`,
+    });
+
+  if (comment.author.id !== req.user.id)
+    return res.status(FORBIDDEN).json({
+      statusCode: FORBIDDEN,
+      message: 'You are not the author',
+    });
+
+  comment.body = req.body.body;
+
+  return res.json(await comment.save());
+};
+
+export default validateMethod(
+  ['GET', 'PUT'],
+  connectDB((req, res) => {
+    switch (req.method) {
+      case 'GET':
+        return showCommentHandler(req, res);
+      case 'PUT':
+        return withAuthentication(
+          validateBody(CommentSchema, editCommentHandler),
+        )(req, res);
+    }
+  }),
+);
