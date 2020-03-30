2fb0dbcb269291cf4d0de5c6104dca14304f7a52
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-29 20:03:12 -0600

    fix(api): Avoid memory leaks and fail quickly
    SEE https://mongoosejs.com/docs/lambda.html

--- middlewares/connectDB.ts
+++ middlewares/connectDB.ts
@@ -12,8 +12,12 @@ export function connectDB(handler: NextHttpHandler): NextHttpHandler {
         useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
+        // Buffering means mongoose will queue up operations if it gets
+        // disconnected from MongoDB and send them when it reconnects.
+        // With serverless, better to fail fast if not connected.
+        bufferCommands: false, // Disable mongoose buffering
+        bufferMaxEntries: 0, // and MongoDB driver buffering
       });
-      mongoose.connection.on('disconnected', handleConnection);
 
       return handler(req, res);
     } catch (error) {
