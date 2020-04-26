5f91fb35c3edbaf25167c2b4767cb31e3d5c981e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-04-25 19:26:08 -0600

    test(e2e): Add error scenaries

--- cypress/integration/auth-api.spec.ts
+++ cypress/integration/auth-api.spec.ts
@@ -1,37 +1,86 @@
 import faker from 'faker';
-import { CREATED, OK } from 'http-status-codes';
+import {
+  CREATED,
+  OK,
+  UNAUTHORIZED,
+  UNPROCESSABLE_ENTITY,
+} from 'http-status-codes';
 
 describe('Auth api', () => {
   describe('Register', () => {
     it('should register a new user', () => {
+      cy.request('POST', '/api/auth/register', {
+        fullName: faker.name.findName(),
+        email: faker.internet.exampleEmail(),
+        password: faker.internet.password(8, true),
+      })
+        .get('status')
+        .should('be', CREATED);
+    });
+
+    it('should validate the payload', () => {
       cy.request({
         url: '/api/auth/register',
         method: 'POST',
         body: {
-          fullName: faker.name.findName(),
-          email: faker.internet.exampleEmail(),
-          password: faker.internet.password(8, true),
+          fullName: 'no',
+          email: 'john',
+          password: 'short',
         },
-        retryOnNetworkFailure: true,
+        failOnStatusCode: false,
       })
         .get('status')
-        .should('be', CREATED);
+        .should('be', UNPROCESSABLE_ENTITY);
     });
   });
 
   describe('Login', () => {
     it('should sign in', () => {
+      cy.request('POST', '/api/auth/login', {
+        email: Cypress.env('USER_EMAIL') || 'john@doe.me',
+        password: Cypress.env('USER_PASSWORD') || 'Pa$$w0rd!',
+      })
+        .get('status')
+        .should('be', OK);
+    });
+
+    it('should validate the payload', () => {
+      cy.request({
+        url: '/api/auth/login',
+        method: 'POST',
+        body: {
+          email: 'john',
+          password: 'short',
+        },
+        failOnStatusCode: false,
+      })
+        .get('status')
+        .should('be', UNPROCESSABLE_ENTITY);
+    });
+
+    it('should fail to sign in', () => {
+      cy.request({
+        url: '/api/auth/login',
+        method: 'POST',
+        body: {
+          email: faker.internet.email(),
+          password: faker.internet.password(16),
+        },
+        failOnStatusCode: false,
+      })
+        .get('status')
+        .should('be', UNAUTHORIZED);
       cy.request({
         url: '/api/auth/login',
         method: 'POST',
         body: {
           email: Cypress.env('USER_EMAIL') || 'john@doe.me',
-          password: Cypress.env('USER_PASSWORD') || 'Pa$$w0rd!',
+          password: faker.internet.password(16),
         },
-        retryOnNetworkFailure: true,
+        failOnStatusCode: false,
       })
         .get('status')
-        .should('be', OK);
+        .should('be', UNAUTHORIZED);
     });
   });
 });
