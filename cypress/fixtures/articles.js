99c6f1b322822e090a44b1fbf4e68ad2b4d36038
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-10-19 14:25:56 -0600

    chore: Add fixtures of article collection

---
+++ cypress/fixtures/articles.js
@@ -0,0 +1,98 @@
+const faker = require('faker');
+const { ObjectID } = require('mongodb');
+
+module.exports = [
+  {
+    _id: ObjectID('5f8d1c1b27b977eec9bc1409'),
+    draft: true,
+    tags: ['war', 'international', 'Azerbaijan', 'Armenia', 'Nagorno-Karabakh'],
+    title: '2020 Nagorno-Karabakh conflict',
+    subtitle: `The 2020 Nagorno-Karabakh conflict is an ongoing armed conflict between Azerbaijan, supported by Turkey, and the self-proclaimed Republic of Artsakh, supported by Armenia, in the disputed Nagorno-Karabakh region. It is the latest escalation of the unresolved Nagorno-Karabakh conflict over the territory, which is internationally recognized as part of Azerbaijan, but mostly governed by Artsakh, a breakaway state with an Armenian ethnic majority.
+Clashes began on the morning of 27 September 2020 along the Nagorno-Karabakh Line of Contact, which had been established in the aftermath of the Nagorno-Karabakh War (1988–1994). In response to the clashes, Armenia and Artsakh introduced martial law and total mobilization, while Azerbaijan introduced martial law and a curfew. On 28 September, partial mobilization was declared in Azerbaijan. The conflict has been marked by the deployment of drones, sensors, long-range heavy artillery and ballistic missile strikes, as well as by state propaganda and the use of official social media accounts in online information warfare.
+Numerous countries and the United Nations strongly condemned the conflict and called on both sides to deescalate tensions and resume meaningful negotiations without delay. Turkey is providing military support to Azerbaijan, although the extent of its support is disputed. International analysts believe that fighting likely began with an Azerbaijani offensive, and that primary goals of the offensive were to capture districts in southern Nagorno-Karabakh that are less mountainous and thus easier to take than the region's well-fortified interior. Turkey's support for Azerbaijan is thought to be an attempt to extend its sphere of influence both by increasing the standing of Azerbaijan in the conflict and by marginalizing Russia's influence over the region.
+A fragile humanitarian ceasefire brokered by Russia, facilitated by the International Committee of the Red Cross, and agreed to by both Armenia and Azerbaijan, formally came into effect on 10 October. The ceasefire has been repeatedly violated, halting a planned return of the dead and exchange of wounded and prisoners. Total casualties on both sides may be approaching the low thousands, and due to Russian-Turkish geopolitical dynamics and pipeline politics there are concerns regarding a wider regional conflict.`,
+    body: `Background
+
+    The clashes are part of the Nagorno-Karabakh conflict over the disputed region of Nagorno-Karabakh, with its ethnic Armenian majority. The region is a de jure part of Azerbaijan, but is de facto held by the self-proclaimed Republic of Artsakh, which is supported by Armenia. Ethnic violence began in the late 1980s, and exploded into a full war following the dissolution of the USSR in 1991. The Nagorno-Karabakh War displaced about 600,000 Azerbaijani residents who used to live in the territory and surrounding provinces. The War ended with a ceasefire in 1994, with the Republic of Artsakh controlling most of the Nagorno-Karabakh region, as well as the surrounding districts of Agdam, Jabrayil, Fuzuli, Kalbajar, Qubadli, Lachin and Zangilan of Azerbaijan.
+    For three decades, multiple violations of the ceasefire have occurred, the most serious incidents prior to the current conflict being the 2016 Nagorno-Karabakh clashes. Long-standing international mediation attempts to create a peace process were initiated by the OSCE Minsk Group in 1994, with the interrupted Madrid Principles being the most recent iteration. While it is unclear how the present inhabitants of the area want to administer the territory, surveys indicate that they do not want to be part of Azerbaijan. In August 2019, in an unprecedented declaration in favour of unification, the Armenian Prime Minister, Nikol Pashinyan, visited Nagorno-Karabakh, stating, "Artsakh is Armenia, full stop".
+    Skirmishes occurred on the border between Armenia and Azerbaijan in July 2020. Thousands of Azerbaijanis demonstrated for war against Armenia in response, with Turkey propagandising in support of Azerbaijan.
+    On 23 July 2020, Armenia announced the start of a joint air defence system exercise with Russia and an analysis of the July 2020 clashes. A week later, Azerbaijan conducted a series of military exercises that lasted from 29 July to 10 August, and further exercises in early September with the involvement of Turkey. Turkey's support for Azerbaijan has been seen as connected to its expansionist, neo-Ottoman foreign policy, linking its intervention to its policies in Syria, Iraq, and the Eastern Mediterranean. Turkey's high visibility role in the conflict has complicated it by raising the matter of the 1.5 million Armenians who died in the deportations, forced marches, and massacres it initiated in 1915.
+    Prior to the resumption of hostilities, allegations emerged that hundreds of Syrian National Army members from the Hamza Division were transferred to Azerbaijan, while Turkish media close to President Erdogan claimed that YPG and PKK members from Iraq and Syria were transferred to Nagorno-Karabakh in order to train Armenian militias against Azerbaijan. The governments of both Azerbaijan and Armenia have denied allegations of involvement by foreign fighters.
+
+Course of the conflict
+
+The conflict began with an Azerbaijani ground offensive that included armored formations, supported by artillery and drones, including loitering munitions. Armenian and Artsakh troops were forced back from their first line of defense in Artsakh's southeast and northern regions, but inflicted significant losses on Azerbaijani armored formations with anti-tank guided missiles and artillery, destroying dozens of vehicles. Azerbaijan made heavy use of drones in strikes against Armenian air defenses, taking out 13 short-range surface-to-air missile systems. Azerbaijani troops managed to make limited gains in the south in the first three days of the conflict. For the next three days, both sides largely exchanged fire from fixed positions. In the north, Armenian/Artsakh forces counterattacked, managing to retake some ground. Their largest counterattack took place on the fourth day, but incurred heavy losses when their armor and artillery units were exposed to Azerbaijani attack drones, loitering munitions, and reconnaissance drones spotting for Azerbaijani artillery as they maneuvered in the open.
+On the sixth day, Azerbaijan and Armenia began trading missile and rocket artillery strikes against infrastructure. Among the targets hit were Stepanakert, the capital of Artsakh, which was repeatedly shelled with rocket artillery, a bridge linking Armenia with Nagorno-Karabakh, which was taken out in a missile strike, and Ganja, which was hit four times by Armenian and Artsakh ballistic missiles, with Ganja International Airport among the targets. On the morning of the seventh day, Azerbaijan launched a major offensive. The Azerbaijani Army's First, Second, and Third Army Corps, reinforced by reservists from the Fourth Army Corps, began an advance in the north, making some territorial gains, but the Azerbaijani advance stalled. Most of the fighting subsequently shifted to the south, in terrain that is relatively flat and underpopulated as compared to the mountainous north. Azerbaijani forces launched offensives toward Jabrayil and Füzuli, managing to recapture a stretch of territory held by Armenian troops as a buffer zone, but the fighting subsequently stalled. Throughout the campaign, Azerbaijan has relied heavily on drones to strike at Armenian/Artsakh forces, and managed to inflict heavy losses. Having successfully targeted tanks, artillery, and air defense systems, Azerbaijani drones are now targeting units of soldiers. However, some Azerbaijani drones have been shot down.
+On 9 October, both sides agreed to a temporary humanitarian ceasefire. After the declared ceasefire, the President of Artsakh admitted Azerbaijan had been able to achieve some success, moving the front deep into Artsakh territory; the Armenian Prime Minister announced that Armenian forces had conducted a "partial retreat".`,
+    author: ObjectID('5e9cce858f8fa801aa70f569'),
+    slug: '2020-nagorno-karabakh-conflict-nxg8n7',
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    __v: 0,
+  },
+  {
+    _id: ObjectID('5f8d211627b977eec9bc140e'),
+    draft: false,
+    tags: ['biography', 'history', 'archeology'],
+    title: 'V. Gordon Childe',
+    subtitle:
+      'Vere Gordon Childe (14 April 1892 – 19 October 1957) was an Australian archaeologist who specialised in the study of European prehistory. He spent most of his life in the United Kingdom, working as an academic for the University of Edinburgh and then the Institute of Archaeology, London. He wrote twenty-six books during his career. Initially an early proponent of culture-historical archaeology, he later became the first exponent of Marxist archaeology in the Western world.',
+    body: `Vere Gordon Childe (14 April 1892 – 19 October 1957) was an Australian archaeologist who specialised in the study of European prehistory. He spent most of his life in the United Kingdom, working as an academic for the University of Edinburgh and then the Institute of Archaeology, London. He wrote twenty-six books during his career. Initially an early proponent of culture-historical archaeology, he later became the first exponent of Marxist archaeology in the Western world.
+Born in Sydney to a middle-class English migrant family, Childe studied classics at the University of Sydney before moving to England to study classical archaeology at the University of Oxford. There, he embraced the socialist movement and campaigned against the First World War, viewing it as a conflict waged by competing imperialists to the detriment of Europe's working class. Returning to Australia in 1917, he was prevented from working in academia because of his socialist activism, instead working for the Labor Party as the private secretary of the politician John Storey. Growing critical of Labor, he wrote an analysis of their policies and joined the far-left Industrial Workers of the World. Emigrating to London in 1921, he became librarian of the Royal Anthropological Institute and journeyed across Europe to pursue his research into the continent's prehistory, publishing his findings in academic papers and books. In doing so he introduced the continental European concept of an archaeological culture—the idea that a recurring assemblage of artefacts demarcates a distinct cultural group—to the British archaeological community.
+From 1927 to 1946 he worked as the Abercromby Professor of Archaeology at the University of Edinburgh, and then from 1947 to 1957 as the director of the Institute of Archaeology, London. During this period he oversaw the excavation of archaeological sites in Scotland and Northern Ireland, focusing on the society of Neolithic Orkney by excavating the settlement of Skara Brae and the chambered tombs of Maeshowe and Quoyness. In these decades he published prolifically, producing excavation reports, journal articles, and books. With Stuart Piggott and Grahame Clark he co-founded The Prehistoric Society in 1934, becoming its first president. Remaining a committed socialist, he embraced Marxism, and—rejecting culture-historical approaches—used Marxist ideas such as historical materialism as an interpretative framework for archaeological data. He became a sympathiser with the Soviet Union and visited the country on several occasions, although he grew sceptical of Soviet foreign policy following the Hungarian Revolution of 1956. His beliefs resulted in him being legally barred from entering the United States, despite receiving repeated invitations to lecture there. Upon retirement, he returned to Australia's Blue Mountains, where he committed suicide.
+One of the best-known and most widely cited archaeologists of the twentieth century, Childe became known as the "great synthesizer" for his work integrating regional research with a broader picture of Near Eastern and European prehistory. He was also renowned for his emphasis on the role of revolutionary technological and economic developments in human society, such as the Neolithic Revolution and the Urban Revolution, reflecting the influence of Marxist ideas concerning societal development. Although many of his interpretations have since been discredited, he remains widely respected among archaeologists. `,
+    author: ObjectID('5e9cce858f8fa801aa70f569'),
+    slug: 'v-gordon-childe-rpl8kh',
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    __v: 0,
+  },
+  {
+    _id: ObjectID('5f8dd90020f31dd4202dbd72'),
+    draft: true,
+    tags: ['branding', 'analytics', 'innovation'],
+    title: 'Funneling branding in order to disrupt the balance',
+    subtitle:
+      'Funneling branding in order to disrupt the balance. Execute big data but innovate. Informing key demographics to in turn get buy in. Engaging integrated tech stacks in order to make the logo bigger. Informing empathy maps and try to use best practice.',
+    body: `Funneling branding in order to disrupt the balance. Execute big data but innovate. Informing key demographics to in turn get buy in. Engaging integrated tech stacks in order to make the logo bigger. Informing empathy maps and try to use best practice.
+Generating scrum masters in order to get buy in. Utilising dark social in order to be CMSable. Engage analytics with the aim to infiltrate new markets. Executing awareness and above all, innovate. Leveraging integrated tech stacks so that as an end result, we make the logo bigger. Leading brand ambassadors yet build ROI.
+Driving dark social but build ROI. Target user stories to, consequently, be on brand. Executing innovation and above all, re-target key demographics. Leading social and then increase viewability. Repurpose customer journeys so that we come up with a bespoke solution.
+Leading big data while remembering to be transparent. Execute user stories yet come up with a bespoke solution. Leverage agile but create a better customer experience. Build stakeholder management with the possibility to think outside the box. Repurpose scrum masters yet come up with a bespoke solution. Repurposing below the fold and then be CMSable.`,
+    author: ObjectID('5e9cce858f8fa801aa70f569'),
+    slug: 'funneling-branding-in-order-to-disrupt-the-balance-h7bs3m',
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    __v: 0,
+  },
+  {
+    _id: ObjectID('5f8ddf0520f31dd4202dbd78'),
+    draft: true,
+    tags: ['agile framework', 'strategy', 'holistic'],
+    title: 'Leverage agile frameworks',
+    subtitle:
+      'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.',
+    body: `Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.
+Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.
+Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.`,
+    author: ObjectID('5e9cd0178f8fa801aa70f56a'),
+    slug: 'leverage-agile-frameworks-cdg7ud',
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    __v: 0,
+  },
+  {
+    _id: ObjectID('5f8deb4420f31dd4202dbd81'),
+    draft: false,
+    tags: faker.random.words().split(' '),
+    title: 'optio et voluptatibus',
+    subtitle: faker.lorem.sentence(),
+    body: faker.lorem.paragraphs(),
+    author: ObjectID('5e9cce858f8fa801aa70f569'),
+    slug: 'optio-et-voluptatibus-stv3jn',
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    __v: 0,
+  },
+];
