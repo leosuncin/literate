44110ce0946b6fd6328b91c13645633619bf7a54
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-05-03 15:57:24 -0600

    fix(comment): check when article not exists

--- pages/api/article/[slug]/comment/index.ts
+++ pages/api/article/[slug]/comment/index.ts
@@ -11,15 +11,16 @@ import { commentCreate, pagination } from 'schemas';
 import { NextHttpHandler, NotFoundError } from 'types';
 
 const createCommentHandler: NextHttpHandler = async (req, res) => {
-  const comment = new Comment(req.body);
   const article = await Article.findOne({ slug: req.query.slug as string });
-  comment.article = article._id;
-  comment.author = req.user._id;
 
   if (!article)
     throw new NotFoundError(
       `Not found any article with slug: ${req.query.slug}`,
     );
+
+  const comment = new Comment(req.body);
+  comment.article = article._id;
+  comment.author = req.user._id;
 
   await comment.save();
 
@@ -29,15 +30,16 @@ const createCommentHandler: NextHttpHandler = async (req, res) => {
 const listCommentHandler: NextHttpHandler = async (req, res) => {
   const { page, size } = await pagination.validate(req.query);
   const article = await Article.findOne({ slug: req.query.slug as string });
-  const comments = await Comment.find({ article: article._id })
-    .populate('author')
-    .limit(size)
-    .skip(size * (page - 1));
 
   if (!article)
     throw new NotFoundError(
       `Not found any article with slug: ${req.query.slug}`,
     );
+
+  const comments = await Comment.find({ article: article._id })
+    .populate('author')
+    .limit(size)
+    .skip(size * (page - 1));
 
   return res.json(comments);
 };
