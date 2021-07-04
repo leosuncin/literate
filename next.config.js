96baed867680040ae30cd2467a66028ae144ea72
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-07-04 11:32:48 -0600

    build: fix next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-    };
-    config.resolve.modules.push(__dirname);
-
     return config;
   },
 };
